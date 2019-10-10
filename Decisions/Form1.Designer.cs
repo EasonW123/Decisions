7812e76fae7abb89817a6859adaab64517a1dba8
Zhi Cheng Wu <zhiwu093@ed.amdsb.ca>
2019-10-10 13:29:04 -0400

    Add project files.

---
+++ Decisions/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Decisions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.ForeColor = System.Drawing.Color.Lime;
+            this.startButton.Location = new System.Drawing.Point(211, 325);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(95, 34);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.ForeColor = System.Drawing.Color.Lime;
+            this.exitButton.Location = new System.Drawing.Point(470, 325);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(93, 34);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Decisions/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; 
+
+namespace Decisions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            // object 
+            Graphics g = this.CreateGraphics();
+            SolidBrush letterBrush = new SolidBrush(Color.White);
+            Font letterFont = new Font("Arial", 15, FontStyle.Regular);
+
+            g.Clear(Color.Black);
+            g.DrawString("Hello World", letterFont, letterBrush, 200, 70);
+            Thread.Sleep(3000);
+            g.DrawString("Hello World", letterFont, letterBrush, 200, 70);
+            g.Clear(Color.Red);
+            Thread.Sleep(350);
+            g.DrawString("Hello World", letterFont, letterBrush, 200, 70);
+            g.Clear(Color.Black);
+            Thread.Sleep(350);
+            g.DrawString("Hello World", letterFont, letterBrush, 200, 70);
+            g.Clear(Color.Red);
+            Thread.Sleep(350);
+            g.DrawString("Hello World", letterFont, letterBrush, 200, 70);
+            g.Clear(Color.Black);
+            Thread.Sleep(350);
+            g.DrawString("Press exit button", letterFont, letterBrush, 200, 70);
+            Thread.Sleep(350);
+            
+
+
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            // object 
+            Graphics g = this.CreateGraphics();
+            SolidBrush letterBrush = new SolidBrush(Color.White);
+            Font letterFont = new Font("Arial", 15, FontStyle.Regular);
+
+            g.Clear(Color.Black);
+            g.DrawString("Good bye", letterFont, letterBrush, 200, 70);
+            Thread.Sleep(2500);
+            this.Close();
+        }
+    }
+}
